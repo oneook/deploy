585e820b66a231abe5fba6be8eb2f2f304cc0db1
oneook <oneook17@163.com>
2019-10-17 11:15:25 +0800

    py3 install update

--- install_py3.sh
+++ install_py3.sh
@@ -2,12 +2,13 @@
 #by fyx
 #centos compile py3
 
-PY3_URL=https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz
+PY3_V=3.6.2
+PY3_URL=https://www.python.org/ftp/python/${PY3_V}/Python-${PY3_V}.tgz
 PY3_DIR=/usr/local/python3
 OS_BIN=/usr/bin
 
 wget ${PY3_URL}
-tar -xf Python-3.6.2.tgz
+tar -xf Python-${PY3_V}.tgz && cd Python-${PY3_V}
 mkdir -p ${PY3_DIR}
 yum -y install gcc* glibc*
 ./configure --prefix=${PY3_DIR}
